60826c9616c72f558e8ee4a467012aee1a1eb53a
Tino Hager <tino.hager@nager.at>
2024-03-12 16:58:02 +0100

    Update vue.config.js

--- src/vue-word-plugin/vue.config.js
+++ src/vue-word-plugin/vue.config.js
@@ -1,16 +1,25 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 const homedir = require('os').homedir()
 
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    port: 3000,
-    https: {
-      key: fs.readFileSync(path.resolve(`${homedir}/.office-addin-dev-certs/localhost.key`)),
-      cert: fs.readFileSync(path.resolve(`${homedir}/.office-addin-dev-certs/localhost.crt`)),
-      ca: fs.readFileSync(path.resolve(`${homedir}/.office-addin-dev-certs/ca.crt`))
-    }
-  }
+  devServer:
+    process.env.NODE_ENV === 'development'
+      ? {
+          port: 3000,
+          https: {
+            key: fs.readFileSync(
+              path.resolve(`${homedir}/.office-addin-dev-certs/localhost.key`)
+            ),
+            cert: fs.readFileSync(
+              path.resolve(`${homedir}/.office-addin-dev-certs/localhost.crt`)
+            ),
+            ca: fs.readFileSync(
+              path.resolve(`${homedir}/.office-addin-dev-certs/ca.crt`)
+            )
+          }
+        }
+      : undefined
 })
